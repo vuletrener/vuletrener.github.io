99be9f03bf55f3f84cd2e5757444e7c050e74b64
vuletrener <vuletrener@gmail.com>
2023-12-08 13:46:21 +0100

    Prepravljen mejl da puca

--- src/app/api/sendEmail/route.js
+++ src/app/api/sendEmail/route.js
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer"
 
 export async function POST(request) {
   try {
-    const {name, contactDetails} = await request.json()
+    const {name, contactDetails, selectedDate} = await request.json()
 
     const transport = nodemailer.createTransport({
       service: 'gmail',
@@ -16,8 +16,16 @@ export async function POST(request) {
     const mailOptions = {
       from: process.env.MY_EMAIL,
       to: process.env.MY_EMAIL,
-      subject: `Message from ${name} (${contactDetails})`,
-      text: contactDetails,
+      subject: `${name} šalje zahtev za trening!`,
+      html: `
+      <h1>Novi trening je zakazan.</h1>
+      <br/><br/>
+      <h2>Ime: ${name}</h2>
+      <h2>Kontakt: ${contactDetails}</h2>
+      <h2>Datum: ${selectedDate}</h2>
+      <br/><br/>
+      <h2>Potvrdi ovaj trening. Srećno!</h2>
+      `,
     };
 
    await transport.sendMail(mailOptions)
